8aa51ca6de3d4eacec4cccb1d62de1eadfd40437
unknown <you@example.com>
2026-05-12 10:20:10 +0530

    change env and port

--- backend/main.py
+++ backend/main.py
@@ -72,4 +72,4 @@ if serve_frontend:
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8010, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
